06e13cc2729aefddf3a99cdce1b9d4238eecf34d
Cristan "Dr.Optix" Dinu <dr.optix@gmail.com>
2014-11-23 13:53:24 +0200

    Set GarbageMapFragment as main fragment for MasterActivity

--- hackaton.agilehub.geoeco.app/src/main/kotlin/hackaton/agilehub/geoeco/app/activities/MasterActivity.kt
+++ hackaton.agilehub.geoeco.app/src/main/kotlin/hackaton/agilehub/geoeco/app/activities/MasterActivity.kt
@@ -2,12 +2,17 @@ package hackaton.agilehub.geoeco.app.activities
 
 import android.app.Activity
 import android.os.Bundle
+import hackaton.agilehub.geoeco.app.R
+import hackaton.agilehub.geoeco.app.fragments.GarbageMapFragment
 
 class MasterActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super<Activity>.onCreate(savedInstanceState)
 
-
+        this.setContentView(R.layout.activity_master)
+        this.getFragmentManager().beginTransaction()
+            .add(R.id.activity_master_fragment_area, GarbageMapFragment())
+            .commit()
     }
 }
